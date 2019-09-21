2403ca389c7b37338d80a0b8d7dfd1e964082bd6
armandoDiaz16 <hondas.el.hondas@gmail.com>
2019-09-21 01:33:34 -0500

    add component heroe, about and their routes in the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,16 @@ import { APP_ROUTING } from './app.routes';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './components/body/body.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BodyComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import { BodyComponent } from './components/body/body.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+
 
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: BodyComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'about', component: AboutComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
